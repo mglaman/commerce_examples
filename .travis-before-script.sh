bd350dfac6d0ff803d284991ce878cb48013d39a
Matt Glaman <nmd.matt@gmail.com>
2016-10-05 08:30:36 -0500

    Fix the build

--- .travis-before-script.sh
+++ .travis-before-script.sh
@@ -2,15 +2,11 @@
 
 set -e $DRUPAL_TI_DEBUG
 
+export DRUPAL_TI_MODULES_PATH="modules/contrib"
+
 # Ensure the right Drupal version is installed.
 # Note: This function is re-entrant.
 drupal_ti_ensure_drupal
-
-# Add custom modules to drupal build.
-cd "$DRUPAL_TI_DRUPAL_DIR"
-
-# Enable main module and submodules.
-drush en -y commerce commerce_product commerce_order commerce_checkout commerce_promotion commerce_payment
 
 # Turn on PhantomJS for functional Javascript tests
 phantomjs --ssl-protocol=any --ignore-ssl-errors=true $DRUPAL_TI_DRUPAL_DIR/vendor/jcalderonzumba/gastonjs/src/Client/main.js 8510 1024 768 2>&1 >> /dev/null &
--- .travis-script.sh
+++ .travis-script.sh
@@ -3,5 +3,7 @@
 set -e $DRUPAL_TI_DEBUG
 
 cd "$DRUPAL_TI_DRUPAL_DIR"
+drush cset migrate_plus.migration_group.commerce_demo_tshirt shared_configuration.source.path ./modules/commerce_demo/data/demo_t_shirts.csv --yes
+drush cset migrate_plus.migration_group.commerce_demo_ebook shared_configuration.source.path ./modules/commerce_demo/data/demo_ebooks.csv --yes
 drush cc drush
 drush mi --all
